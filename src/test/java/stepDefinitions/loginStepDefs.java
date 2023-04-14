06f5382f1ab4f8921619d374ec9a17105ea5bd3f
rigshan.prabakaran <rigshan.prabakaran@ten10.com>
2023-04-14 12:41:47 +0100

    added steps to sign in

--- src/test/java/stepDefinitions/loginStepDefs.java
+++ src/test/java/stepDefinitions/loginStepDefs.java
@@ -1,6 +1,11 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import pageObjects.AccountPage;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
 
@@ -15,5 +20,11 @@ public class loginStepDefs {
     @Given("I am on the homepage")
     public void iAmOnHomePage(){
         homePage.goToHomepage();
+        System.out.println("I am on the homepage");
+    }
+    @When("I click on Sign In")
+    public void clickSignIn(){
+        homePage.clickSignIn();
+        System.out.println("I am on the sign in page");
     }
 }
