8aaa0fb6326c6b4ca66e2e835159bb72521ddd60
rigshan.prabakaran <rigshan.prabakaran@ten10.com>
2023-04-14 12:44:13 +0100

    removed unused imports and reformatted

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,19 +1,19 @@
 package pageObjects;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 
 public class HomePage extends BasePage {
-    private static final String URL=("http://3.11.77.136/index.php");
+    private static final String URL = ("http://3.11.77.136/index.php");
 
     private static final By CLICK_SIGN_IN_BUTTON = By.xpath("//span[text()='Sign in']");
-//            By.cssSelector(".user-info > a");
-    public void goToHomepage(){
+
+    //            By.cssSelector(".user-info > a");
+    public void goToHomepage() {
         driver.get(URL);
     }
-    public void clickSignIn(){
+
+    public void clickSignIn() {
         waitAndClick(CLICK_SIGN_IN_BUTTON);
     }
 
