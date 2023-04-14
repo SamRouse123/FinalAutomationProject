cb3f39af6f6d800bf65edb7e78950f01480d0b81
mohammed.el-nur <mohammed.el-nur@ten10.com>
2023-04-14 15:59:42 +0100

    created link for dress

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -2,8 +2,11 @@ package pageObjects;
 
 public class HomePage extends BasePage {
     private static final String URL=("http://3.11.77.136/index.php");
+    private static final By DRESS_LINK = By.xpath("//img[@src='http://3.11.77.136/img/p/1/1-home_default.jpg']");
     public void goToHomepage(){
         driver.get(URL);
     }
-
+    public void clickOnDress(){
+waitAndClick(DRESS_LINK);
+    }
 }
