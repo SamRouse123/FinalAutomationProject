b37b6fb2095e74c5d33e4280ecbcd444d37c10db
samuel.rouse <samuel.rouse@ten10.com>
2023-04-14 15:54:40 +0100

    adding get text method

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -1,6 +1,8 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import stepDefinitions.Hooks;
 
